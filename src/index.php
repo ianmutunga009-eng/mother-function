80cf453ae7c8456ad9934ee9f04b7990f287304f
Ian Munguti <226789459+ianmutunga009-eng@users.noreply.github.com>
2025-08-20 21:46:56 +0300

    6

--- src/index.php
+++ src/index.php
@@ -1,11 +1,34 @@
 <?php
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+use Appwrite\Client;
+use Appwrite\Services\Users;
+
 return function ($context) {
-    return $context->res->json([
-        'status' => 'success',
-        'message' => 'Appwrite Function is alive 🚀',
-        'timestamp' => date('Y-m-d H:i:s'),
-        'method' => $context->req->method,
-        'query' => $context->req->query,
-        'input' => $context->req->body
-    ]);
+    $client = new Client();
+    $client
+        ->setEndpoint(getenv('APPWRITE_FUNCTION_API_ENDPOINT'))
+        ->setProject(getenv('APPWRITE_FUNCTION_PROJECT_ID'))
+        ->setKey($context->req->headers['x-appwrite-key']);
+
+    $users = new Users($client);
+
+    try {
+        $response = $users->list();
+        $context->log('Total users: ' . $response['total']);
+
+        return $context->res->json([
+            'status' => 'success',
+            'totalUsers' => $response['total'],
+            'users' => $response['users']
+        ]);
+    } catch (Throwable $error) {
+        $context->error('Could not list users: ' . $error->getMessage());
+
+        return $context->res->json([
+            'status' => 'error',
+            'message' => $error->getMessage()
+        ]);
+    }
 };
